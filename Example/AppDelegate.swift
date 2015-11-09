74bb9c536b3370b5562a1fc7f5a728027d23ad51
Dylan Bettermann <dylan.bettermann@gmail.com>
2015-11-09 12:42:09 -0600

    Moving files around and adding a true framework project/target

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import DBAlertController
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
---
+++ Source/DBAlertController.h
@@ -0,0 +1,19 @@
+//
+//  DBAlertController.h
+//  DBAlertController
+//
+//  Created by Dylan.Bettermann on 11/9/15.
+//  Copyright © 2015 Dylan Bettermann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DBAlertController.
+FOUNDATION_EXPORT double DBAlertControllerVersionNumber;
+
+//! Project version string for DBAlertController.
+FOUNDATION_EXPORT const unsigned char DBAlertControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DBAlertController/PublicHeader.h>
+
+
